53f0da340c9cbc5ee9790307fad8b109fc298fea
diegooliveyra <diegooliveirabryto@gmail.com>
2021-10-20 17:49:57 -0300

    criado e configurado service de dependencia

--- src/app/department-form/department-form.component.ts
+++ src/app/department-form/department-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DepartmentService } from '../services/department.service';
 
 @Component({
   selector: 'app-department-form',
@@ -9,17 +10,20 @@ export class DepartmentFormComponent implements OnInit {
 
   public depName!: string
 
-  constructor() { }
+  constructor(private service: DepartmentService) { }
 
   ngOnInit(): void {
   }
 
   save(){
-
+    this.service.addDepartments({
+      name: this.depName
+    })
+    this.clear()
   }
 
   clear(){
-
+    this.depName = ''
   }
 
 }
---
+++ src/app/services/department.service.ts
@@ -0,0 +1,28 @@
+import { Department } from './../models/department.model';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DepartmentService {
+
+  private departments: Department[] = [
+    {id:1, name:'Clothing'},
+    {id:2, name:'Books'},
+    {id:3, name:'Electronics'},
+    {id:4, name:'Computers'}
+  ]
+  private idNew: number = 5
+  constructor() { }
+
+  getDepartments(): Department[] {
+    return this.departments
+  }
+
+  addDepartments(d:Department){
+    this.departments.push({...d, id:this.idNew++})
+    console.log(this.departments)
+  }
+
+
+}
